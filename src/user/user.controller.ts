4bd317c9e4f4f37c85b6bbe0713c36c13b2142eb
Leonardo <leowitcroz@gmail.com>
2024-03-04 17:55:37 -0300

    PT2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,35 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
 
+    constructor (private readonly userServices: UserService) { }
+
     @Post()
     async create(@Body() { email, name, password }: CreateUserDto) {
-        return { email, name, password }
+        return this.userServices.create( { email, name, password } )
     }
 
     @Get()
     async read() {
-        return []
+        return this.userServices.read()
     }
 
     @Get(':id')
     async readOne(@Param('id', ParseIntPipe) id: number) {
-        return { id }
+        return this.userServices.readOne(id)
     }
 
     @Patch(':id')
     async update(@Param('id', ParseIntPipe) id: number, @Body() { email, name, password } ){
-        return { email, name, password }
+        return this.userServices.update(id, { email, name, password } )
     }   
 
-    @Delete('id')
+    @Delete(':id')
     async delete(@Param('id', ParseIntPipe) id: number){
-
+        return this.userServices.delete(id)
     }
     
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(private readonly prisma: PrismaService) { }
+
+    async create({ email, name, password }: CreateUserDto) {
+        return await this.prisma.users.create({
+            data: {
+                email,
+                name,
+                password
+            }
+        })
+    }
+
+    async read() {
+        return await this.prisma.users.findMany()
+    }
+
+    async readOne(id: number) {
+        return await this.prisma.users.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async update(id: number, { email, name, password }) {
+
+        await this.exists(id)
+
+        return await this.prisma.users.update({
+            where: {
+                id
+            },
+            data: {
+                email,
+                name,
+                password
+            }
+        })
+    }
+
+    async delete(id: number) {
+
+        await this.exists(id)
+
+        return this.prisma.users.delete({
+            where: {
+                id
+            }
+        })
+      
+    }
+
+    async exists(id: number) {
+        if (!(await this.readOne(id))) {
+            throw new NotFoundException('usuario não existe')
+        }
+    }
+
+}
